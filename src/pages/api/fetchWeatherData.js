96338b8bcd6a4c6d98e05f768916471f82b03146
ricardoyeah <ricardohermoginojr@gmail.com>
2025-02-09 03:22:27 +0800

    updated

--- src/pages/api/fetchWeatherData.js
+++ src/pages/api/fetchWeatherData.js
@@ -11,18 +11,14 @@ const dbConfig = {
 };
 
 export default async function fetchWeatherDataHandler(req, res) {
-    // Allow multiple origins
-    const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001',];
-    const origin = req.headers.origin;
-    
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-  
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
 
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
@@ -60,47 +56,3 @@ export default async function fetchWeatherDataHandler(req, res) {
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
-
-
-
-// import WeatherAPI from '../../utils/WeatherApi';
-// import WeatherData from '../../utils/WeatherData';
-// import { locationCoordinates } from '@/utils/locationCoordinates';
-
-// const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
-// const dbConfig = {
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// };
-
-// export default async function fetchWeatherDataHandler(req, res) {
-//   if (req.method === 'POST') {
-//     try {
-//       const weatherAPI = new WeatherAPI(API_KEY);
-//       const weatherDataHandler = new WeatherData(dbConfig);
-
-//       // Fetch weather data for each location
-//       const weatherData = await Promise.all(
-//         Object.entries(locationCoordinates).map(async ([location, coords]) => {
-//           const { lat, lon } = coords;
-//           return await weatherAPI.fetchWeatherData(lat, lon, location);
-//         })
-//       );
-
-//       // Flatten the array to combine all location forecasts
-//       const processedData = weatherData.flat();
-
-//       // Delete old data and insert new data into MySQL
-//       await weatherDataHandler.clearAndInsertWeatherData(processedData);
-
-//       res.status(200).json(processedData);
-//     } catch (error) {
-//       console.error('Error fetching or inserting forecast data:', error);
-//       res.status(500).json({ error: 'Failed to fetch or insert forecast data for some locations' });
-//     }
-//   } else {
-//     res.status(405).json({ message: 'Method not allowed' });
-//   }
-// }
