940de95591e36285fb650fb7343e11c9444ae49d
ricardoyeah <ricardohermoginojr@gmail.com>
2025-02-09 03:47:29 +0800

    updatedgetweather

--- src/pages/api/fetchWeatherData.js
+++ src/pages/api/fetchWeatherData.js
@@ -17,7 +17,7 @@ export default async function fetchWeatherDataHandler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Content-Type, Authorization'
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
 
   // Handle preflight requests
@@ -56,3 +56,47 @@ export default async function fetchWeatherDataHandler(req, res) {
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
+
+
+
+// import WeatherAPI from '../../utils/WeatherApi';
+// import WeatherData from '../../utils/WeatherData';
+// import { locationCoordinates } from '@/utils/locationCoordinates';
+
+// const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+// const dbConfig = {
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// };
+
+// export default async function fetchWeatherDataHandler(req, res) {
+//   if (req.method === 'POST') {
+//     try {
+//       const weatherAPI = new WeatherAPI(API_KEY);
+//       const weatherDataHandler = new WeatherData(dbConfig);
+
+//       // Fetch weather data for each location
+//       const weatherData = await Promise.all(
+//         Object.entries(locationCoordinates).map(async ([location, coords]) => {
+//           const { lat, lon } = coords;
+//           return await weatherAPI.fetchWeatherData(lat, lon, location);
+//         })
+//       );
+
+//       // Flatten the array to combine all location forecasts
+//       const processedData = weatherData.flat();
+
+//       // Delete old data and insert new data into MySQL
+//       await weatherDataHandler.clearAndInsertWeatherData(processedData);
+
+//       res.status(200).json(processedData);
+//     } catch (error) {
+//       console.error('Error fetching or inserting forecast data:', error);
+//       res.status(500).json({ error: 'Failed to fetch or insert forecast data for some locations' });
+//     }
+//   } else {
+//     res.status(405).json({ message: 'Method not allowed' });
+//   }
+// }
--- src/pages/api/getWeatherData.js
+++ src/pages/api/getWeatherData.js
@@ -13,17 +13,19 @@ export default async function getWeatherDataHandler(req, res) {
   try {
     connection = await mysql.createConnection(dbConfig);
 
-     // Allow multiple origins
-  const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
-  const origin = req.headers.origin;
-  
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // CORS headers
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
     if (req.method === 'GET') {
       const { weather_data_id } = req.query;
